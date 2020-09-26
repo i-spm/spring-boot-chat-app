c3ea66d1225efa9b4e31b3cca3edcc16794f7856
i-spm <shivspm54@gmail.com>
2020-09-26 21:24:45 +0530

    initial commit

---
+++ src/main/java/com/chataapforspring/chatappspringboot/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.chataapforspring.chatappspringboot.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import com.chataapforspring.chatappspringboot.messageing.Greeting;
+import com.chataapforspring.chatappspringboot.messageing.HelloMessage;
+
+@Controller
+public class GreetingController {
+
+	@MessageMapping("/hello")
+	@SendTo("/topic/greetings")
+	public Greeting greeting(HelloMessage message) throws Exception {
+		Thread.sleep(1000); // simulated delay
+		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+	}
+
+}
---
+++ src/test/java/com/chataapforspring/chatappspringboot/ChatappspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chataapforspring.chatappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
